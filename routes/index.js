f89018c91a558412b86e14aae7e1552a1bf3ea8f
makaronma <mckahung250@gmail.com>
2022-01-25 18:48:53 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const axios = require("axios");
-require("dotenv").config();
 const API_KEY = process.env.API_KEY;
 let word;
 
