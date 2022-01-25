bcd87fc2a31164bd8ef33f3b9a60d9651b13d5a1
makaronma <mckahung250@gmail.com>
2022-01-25 18:52:56 +0800

    test

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use(express.static("public"));
 
 app.use("/", indexRouter);
 
-// app.listen(process.env.PORT || 3000, () =>
-//   console.log(`listen to port ${process.env.PORT || 3000}`)
-// );
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`listen to port ${process.env.PORT || 3000}`)
+);
