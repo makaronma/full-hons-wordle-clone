bc83ba4c04dc9ac3ff91ecfda3609e8f04b66e09
makaronma <mckahung250@gmail.com>
2022-01-26 13:18:24 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,21 @@ const API_KEY = process.env.API_KEY;
 let word;
 
 // app.listen(process.env.PORT || 3000, async () => {
-//   try {
-//     const result = await axios.get(
-//       `http://api.wordnik.com/v4/words.json/randomWord?api_key=${API_KEY}&&minLength=5&maxLength=5`
-//     );
-//     word = result.data.word;
-//     console.log("Word: " + word);
-//   } catch (error) {
-//     console.log(error);
-//   }
+
 // });
 
 router.get("/", async (req, res) => {
-  res.render("index", { word: word });
+  try {
+    const result = await axios.get(
+      `http://api.wordnik.com/v4/words.json/randomWord?api_key=${API_KEY}&&minLength=5&maxLength=5`
+    );
+    word = result.data.word;
+    console.log("Word: " + word);
+    res.render("index", { word: word });
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 });
 
 const getRandomWord = async () => {};
